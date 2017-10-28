a73be01a6e9e08ce620c2cd9cfc15be193c10a61
nine-god <191176233@qq.com>
2017-10-28 17:39:07 +0800

    网站初步有个轮廓

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone :local
 
+    config.assets.paths << Rails.root.join("lib")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'home/show'
+  get "home/about"
+
   get 'welcome/index'
-  root "welcome#index"
+  root "home#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
