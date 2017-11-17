85b7044cfc506c0086fd5e8a060a5be9426df7a7
nine-god <191176233@qq.com>
2017-11-17 14:42:56 +0800

    active_support 使用东八区

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,14 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Beijing'
-    config.active_record.default_timezone :local
+    config.active_record.default_timezone = :local
 
     config.assets.paths << Rails.root.join("lib")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-      config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   end
 end
