689d9d99b5408fa1fead94db2c017a2f29048200
nine-god <191176233@qq.com>
2018-05-27 16:52:05 +0800

    增加QQ登录图片

--- db/migrate/20180527066526_change_email_users.rb
+++ db/migrate/20180527066526_change_email_users.rb
@@ -1,7 +1,7 @@
 class ChangeEmailUsers < ActiveRecord::Migration[5.1]
   def up
     change_table :users do |t|
-      t.change :email, :string,null: true
+      t.change :email, :string,null: true, default: nil
     end
   end
  
